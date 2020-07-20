a867a1ee2c7a6f80fe13388c7c1ab9df08a3c9a6
Melena Teclai <melenateclai@melenas-mbp.attlocal.net>
2020-07-20 18:09:43 -0400

    made it so that my header stays in place as one scrolls down the page. I also darkened the version of blue on my page.

--- app.js
+++ app.js
@@ -56,3 +56,13 @@ document.getElementById("menu-bg").classList.toggle("change-bg");
 }
 
 
+/* When the user scrolls down 20px from the top of the document, slide down the navbar */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementByClassName("container1").style.top = "0";
+  } else {
+    document.getElementByClassName("container1").style.top = "-50px";
+  }
+}
